a838ddd915f83a9b0691cb3ecb4a488ac89b5529
Yesid Rincon <dairo.rincon@uptc.edu.co>
2023-02-25 10:14:34 -0500

    adding card tour

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,6 +7,9 @@ import heroVideo from '../assets/images/hero-video.mp4'
 import Subtitle from '../shared/Subtitle'
 import worldIMG from '../assets/images/world.png'
 import SearchBar from '../shared/SearchBar';
+import ServiceList from '../services/ServiceList';
+
+import FeaturedTourList from '../components/Featured-tours/FeaturedTourList';
 class Home extends Component {
     render() {
         return <>
@@ -42,6 +45,30 @@ class Home extends Component {
                 </Row>
             </Container>
         </section>
+        <section>
+            <Container>
+                <Row>
+                    <Col lg ='3'>
+                        <h5 className='services__subtitle'> what we serve</h5>
+                        <h2 className='services__title'> we offer our best services</h2>
+                    </Col>
+                    <ServiceList/>
+                </Row>
+            </Container>
+        </section>
+        {/**========================== featured tour section start=================== */}
+        <section>
+            <Container>
+                <Row>
+                    <Col lg = "12" className='mb-5'>
+                    <Subtitle  subtitle={"Explore"}/>
+                    <h2 className='featured__tour-title'>Our featured tours</h2>
+                    </Col>
+                    <FeaturedTourList></FeaturedTourList>
+                </Row>
+            </Container>
+        </section>
+        {/**========================== featured tour section end=================== */}
         </>
     }
 }
--- frontend/src/shared/SearchBar.js
+++ frontend/src/shared/SearchBar.js
@@ -6,33 +6,42 @@ const SearchBar = () => {
     const locationRef = useRef('')
     const distanceRef = useRef(0)
     const maxGroupSizeRef = useRef(0)
+    const searchHandler=()=>{
+        const location = locationRef.current.value
+        const distance = locationRef.current.value
+        const maxGroupSize = locationRef.current.value
+        if(location===''||distance==='' || maxGroupSize ===''){
+            return alert('all fields are required!')
+        }
+    }
 
         return (
         <Col lg="12">
         <div className = "search__bar">
             <Form className='d-flex align-items-center gap-4'>
+                
                 <FormGroup className='d-flex gap-3 form__group form__group-fast'>
                     <span ><i class="ri-map-pin-line"></i> </span>
                     <div>
                         <h6>Location</h6>
-                        <input type = "text" placeholder='where are you going'></input>
+                        <input type = "text" placeholder='where are you going' ref={locationRef}></input>
                     </div>
                 </FormGroup>
                 <FormGroup className='d-flex gap-3 form__group form__group-fast'>
                     <span ><i class="ri-pin-distance-line"></i> </span>
                     <div>
                         <h6>Distance</h6>
-                        <input type = "number" placeholder='Distance k/m'></input>
+                        <input type = "number" placeholder='Distance k/m' ref={distanceRef}></input>
                     </div>
                 </FormGroup>
                 <FormGroup className='d-flex gap-3 form__group form__group-last'>
                     <span ><i class="ri-group-line"></i> </span>
                     <div>
                         <h6>max people</h6>
-                        <input type = "number" placeholder='0'></input>
+                        <input type = "number" placeholder='0' ref={maxGroupSizeRef}></input>
                     </div>
                 </FormGroup>
-                <span className="search__icon" type="submit">
+                <span className="search__icon" type="submit" onClick={searchHandler}>
                 <i class="ri-search-line"></i>
                 </span>
             </Form>
